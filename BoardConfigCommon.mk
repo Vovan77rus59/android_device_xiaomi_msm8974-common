7c68c9c1afe09234680476ac2ad1e31c363067d8
Владимир <vova.perm@hotmail.com>
2019-06-21 01:46:54 +0500

    msm8974-common: Enable boot and system server dex-preopt

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -81,6 +81,16 @@ TARGET_PROCESS_SDK_VERSION_OVERRIDE := \
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
 
+# Dex
+ifeq ($(HOST_OS),linux)
+  ifneq ($(TARGET_BUILD_VARIANT),eng)
+    ifeq ($(WITH_DEXPREOPT),)
+      WITH_DEXPREOPT := true
+      WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
+    endif
+  endif
+endif
+
 # Crypto
 TARGET_HW_DISK_ENCRYPTION := true
 TARGET_LEGACY_HW_DISK_ENCRYPTION := true
